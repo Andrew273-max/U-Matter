4faff984a8b7a180dfc197c3b75722a08ff6bd5e
Andrew273-max <vfgbghnhj@gmail.com>
2026-06-16 22:27:50 +0200

    Designed the event list page

--- v1/event_list.php
+++ v1/event_list.php
@@ -13,60 +13,68 @@
     <link rel="stylesheet" href="styles.css">
     <link rel ="icon" type="image" href="assets/icon.png">
 </head>
-<body class="eventfund">
-    <header>
-        <div class="topnav">
-            <a href="index.html" class="active"><img class="back" src="assets/icon.png"></a>
-            <div id="myLinks">
-                <a href="login.php" id="authLink">Log in</a>
-                <a href="fund_list.php">Funds</a>
-                <a href="event_list.php">Events</a>
-                <a href="contact.html">Contact us</a>
+<body>
+    <div class="app">
+        <header>
+            <div class="topnav">
+                <a href="index.html" class="active"><img class="back" src="assets/icon.png"></a>
+                <div id="myLinks">
+                    <a href="index.html">Home</a>
+                    <a href="login.php" id="authLink">Log in</a>
+                    <a href="fund_list.php">Funds</a>
+                    <a href="event_list.php">Events</a>
+                    <a href="contact.html">Contact us</a>
+                </div>
+                <a href="javascript:void(0);" class="menu" onclick="myFunction()">
+                    <i class="fa fa-bars"></i>
+                </a>
             </div>
-            <a href="javascript:void(0);" class="menu" onclick="myFunction()">
-                <i class="fa fa-bars"></i>
-            </a>
+        </header>
+        <div class="hero">
+            <h1>U Matter</h1>
+            <h3>Organize. Protest. Win</h3>
         </div>
-    </header>
-    <div class="hero">
-        <h1>U Matter</h1>
-        <h3>Organize. Protest. Win</h3>
-    </div>
-    <div class="info-search">
-        <h3>Current volunteering events</h3>
-        <p>Every person to attend makes an impact.<br>Thank you for your support!</p>
-        <div class="search-bar">
-            <form action="/search" method="GET">
-                <input type="text" name="q" placeholder="Search events...">
-                <button type="submit">
-                <img src="assets/search.png" alt="submit" width="10" height="10">
-                </button>
-            </form>
+        <div class="info-search">
+            <h3>Current volunteering events</h3>
+            <p>Every person to attend makes an impact.<br>Thank you for your support!</p>
+            <div class="search-bar">
+                <form action="/search" method="GET">
+                    <input type="text" name="q" placeholder="Search events...">
+                    <button type="submit">
+                    <img src="assets/search.png" alt="submit" width="10" height="10">
+                    </button>
+                </form>
+            </div>
         </div>
-    </div>
-    <ul id="event-list" class="fund-event-list">
-        <?php
+        <ul id="event-list" class="event-list">
+            <?php
             while($row = $result->fetch_assoc()) {
             echo "
-            <a href='event_info.php?id={$row['id']}' class='fund-link'>
-            <li class='fund-card'>
-                <div class='fund-card-image'></div>
-                <div class='fund-card-overlay'>
-                    <h3>{$row['name']}</h3>
-                </div>
+            <li class='event-card'>
+                <a href='event_info.php?id={$row['id']}' class='event-link'>
+                    <img 
+                        src='{$row['photo']}' 
+                        alt='{$row['name']}'
+                        class='event-image'
+                    >
+                    <div class='overlay'></div>
+                    <div class='event-info'>
+                        <h3>{$row['name']}</h3>
+                        <span>" . date("Y-m-d H:i", strtotime($row['date_time'])) . "</span>
+                    </div>
+                </a>
             </li>
-            </a>
             ";
             }
-        ?> 
-    </ul>
-    <div class="btn">
-        <a href="create_event.html">Add a new event</a>
+            ?>
+        </ul>
+        <div class="btn">
+            <a href="create_event.php">Add a new event</a>
+        </div>
+        <footer>
+            <p>U Matter © 2026 All rights reserved</p>
+        </footer>
     </div>
-    <footer>
-        <p>U Matter © 2026 All rights reserved</p>
-    </footer>
-    
 <script src="js/shared/hamburger_menu.js"></script>
 </body>
 </html>
